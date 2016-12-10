78013cd451e77690647cddaf99134e695edbfadd
Jose Castro <jcastro581@alumno.uned.es>
2016-12-10 08:54:36 -0600

    visual.js fixed

--- public/js/visual.js
+++ public/js/visual.js
@@ -3,11 +3,7 @@ var visualRecognitionService = (function () {
   	// Service properties
   	var api_key = "ae710865420b1a8e59a8e47ccdc15b56027312b1";
 	var service_url = "https://gateway-a.watsonplatform.net/visual-recognition/api";
-<<<<<<< Upstream, based on 6ca89a12e2f87ca489aa9368c8223421914d2e0b
 	var end_point = "/v3/classify";
-=======
-	var end_point = "v3/classify";
->>>>>>> c0aff2e js Dir changed. final_url "api/v3" fixed
 	var version = "2016-05-20";
 
 	return {
