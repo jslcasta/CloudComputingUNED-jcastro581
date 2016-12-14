02a4356d535e5976dedb12977b19068d444444be
jslcasta <jlca_92leon@hotmail.com>
2016-12-15 00:56:44 +0100

    Added detect_faces

--- public/js/visual.js
+++ public/js/visual.js
@@ -1,4 +1,4 @@
-var visualRecognitionService = (function () {
+var visualRecognitionService_clasiffy = (function () {
     
   	// Service properties
   	var api_key = "ae710865420b1a8e59a8e47ccdc15b56027312b1";
@@ -9,6 +9,26 @@ var visualRecognitionService = (function () {
 	return {
 			// This function use the jQuery library to do the request
     		clasiffy: function(image_url, ui_function) {
+      			var final_url = service_url + end_point + "?api_key=" + api_key + "&url=" + image_url + "&version=" + version; 
+            	$.get( final_url, function( data ) {
+					ui_function(data);
+				});
+    	}
+	}
+
+})();
+
+var visualRecognitionService_detectFaces = (function () {
+    
+  	// Service properties
+  	var api_key = "ae710865420b1a8e59a8e47ccdc15b56027312b1";
+	var service_url = "https://gateway-a.watsonplatform.net/visual-recognition/api";
+	var end_point = "/v3/detect_faces";
+	var version = "2016-05-20";
+
+	return {
+			// This function use the jQuery library to do the request
+    		detect_faces: function(image_url, ui_function) {
       			var final_url = service_url + end_point + "?api_key=" + api_key + "&url=" + image_url + "&version=" + version; 
             	$.get( final_url, function( data ) {
 					ui_function(data);
